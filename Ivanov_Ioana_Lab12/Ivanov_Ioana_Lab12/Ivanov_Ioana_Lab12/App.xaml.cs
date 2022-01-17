64c75546532661ad6e07e590b83c6a82d5953d43
ioana.ivanov9@gmail.com <ioana.ivanov9@gmail.com>
2022-01-17 05:06:54 +0200

    Add project files.

---
+++ Ivanov_Ioana_Lab12/Ivanov_Ioana_Lab12/Ivanov_Ioana_Lab12/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Ivanov_Ioana_Lab12.Services;
+using Ivanov_Ioana_Lab12.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Ivanov_Ioana_Lab12.Data;
+
+namespace Ivanov_Ioana_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database {get; private set;}
+
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
